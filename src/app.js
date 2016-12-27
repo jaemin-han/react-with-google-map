31e13fc60658ffb790065a51ee9a4c672b45e027
Jaemin Han <jaemin.han@nyu.edu>
2016-12-27 00:43:07 -0500

    google map api insert - map component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
+import Map from './components/Map'
+import Places from './components/Places'
 
 class App extends Component {
   render() {
+
+    const location = {
+      lat: 40.7575285,
+      lng: -73.9884469
+    }
+
     return(
       <div>
         This is the REACT APP!!
+        <div style={{width:300, height:600, background:'red'}}>
+          <Map center={location} />
+        </div>
+        <Places />
       </div>
     )
   }
 }
 
-ReactDOM.react(<App />, document.getElementById('app'))
+// Find the div with this specific id -- takes over this div and render whatever REACT we have
+ReactDOM.render(<App />, document.getElementById('app'))
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps'
+// Import sub-modules from the library (pieces - visual components [googlemap and marker])
 
 class Map extends Component {
   render(){
+    const mapContainer = <div style={{height: '100%', width: '100%'}}></div>
+
     return (
-      <div>This is the Map Component</div>
+      <GoogleMapLoader
+        containerElement = { mapContainer }
+        googleMapElement = {
+          <GoogleMap
+            defaultZoom={15}
+            defaultCenter={this.props.center}
+            options={{streetViewControl: false, mapTypeControl: false}}>
+          </GoogleMap>
+        } />
     )
   }
 }
