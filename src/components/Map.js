12c2ebc04a99d929d1696deb10157a00df4c230f
Jaemin Han <jaemin.han@nyu.edu>
2016-12-26 23:45:39 -0500

    map and places components initial update

---
+++ src/components/Map.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+  render(){
+    return (
+      <div>This is the Map Component</div>
+    )
+  }
+}
+
+// This component will not directly interface with the HTML (DOM)
+export default Map
---
+++ src/components/Places.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Places extends Component {
+  render(){
+    return (
+      <div>This is the Places Component</div>
+    )
+  }
+}
+
+// This component will not directly interface with the HTML (DOM)
+export default Places
